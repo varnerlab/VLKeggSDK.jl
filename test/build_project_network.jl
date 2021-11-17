eb312ee7fb2dccc79d93828b4354079a3ecd203a
Varnerlab <jdv27@cornell.edu>
2021-11-17 13:15:59 -0500

    Update build_project_network.jl
    added test method

--- test/build_project_network.jl
+++ test/build_project_network.jl
@@ -26,8 +26,9 @@ for (index,ec_number) in enumerate(ec_number_array)
     if (isnothing(rxn_object) == false)
         append!(reaction_obj_array, rxn_object)
     end
-
 end
+
+
 
 # now that we have the list of ec numbers -> build a reaction table from these ec numbers
 # reaction_table = get_reactions_for_ec_number(ec_number_array) |> check |> DataFrame
