30485328beb0e5acfe4c589cd0aa5acde83cea77
Jeffrey Varner <jdv27@cornell.edu>
2021-11-19 06:41:15 -0500

    Update test_build_metabolite_keggid_matching_table.jl
    need to build array

--- test/test_build_metabolite_keggid_matching_table.jl
+++ test/test_build_metabolite_keggid_matching_table.jl
@@ -6,16 +6,17 @@ using DataFrames
 metabolite_matching_table = build_metabolite_keggid_matching_table() |> check
 
 # compute the master compound table -
-master_compound_array = Array{KEGGCompound,1}()
-value_itr = values(metabolite_matching_table)
-@showprogress for kegg_compound_code in value_itr
+# master_compound_array = Array{KEGGCompound,1}()
+# value_itr = values(metabolite_matching_table)
+# test_codes = value_itr |> collect
+# @showprogress for kegg_compound_code in test_codes[1:10]
 
-    # mod_code -
-    mod_compund_code = "cpd:$(kegg_compound_code)"
+#     # mod_code -
+#     mod_compund_code = "cpd:$(kegg_compound_code)"
 
-    # grab from cloud -
-    kegg_compound_obj = get_compound_records(mod_compund_code) |> check
-    if (isnothing(kegg_compound_obj) == false)
-        push!(master_compund_array, kegg_compound_obj)
-    end
-end
+#     # grab from cloud -
+#     kegg_compound_obj = get_compound_records(mod_compund_code) |> check
+#     if (isnothing(kegg_compound_obj) == false)
+#         push!(master_compound_array, kegg_compound_obj)
+#     end
+# end
