e48d134d39c2a9a95a778a7c8d13e3fa21376e61
Varnerlab <jdv27@cornell.edu>
2021-11-25 10:05:26 -0500

    working on ox phose
    ox phose

--- test/test_vff_network_build.jl
+++ test/test_vff_network_build.jl
@@ -2,7 +2,7 @@ using VLKeggSDK
 using DataFrames
 
 # path -
-path_to_vff_file = "./test/network/Test.vff"
+path_to_vff_file = "./test/network/Model.vff"
 
 # load the file -
 list_of_reactions = build_reaction_table_from_vff_file(path_to_vff_file) |> check |> DataFrame
